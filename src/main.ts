9b103433555e38e8eddcd2510d03bd930fb0dfdd
Eugene <evgenijantonenkov456@gmail.com>
2024-10-27 00:20:10 +0300

    Add toast notification

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import './index.css'
+import 'vue-toast-notification/dist/theme-bootstrap.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
+import ToastPlugin from 'vue-toast-notification'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,5 +16,6 @@ const app = createApp(App)
 
 app.use(router)
 app.use(createPinia())
+app.use(ToastPlugin)
 
 app.mount('#app')
